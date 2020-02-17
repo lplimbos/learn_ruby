a6fd69701fa906a28fc9bc6a7aab97f60136ecc4
lplimbos <lplimbos@gmail.com>
2020-02-16 21:55:54 -0600

    Solved 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,43 @@
 #write your code here
+def add(num1, num2)
+    res = num1 + num2
+    return res
+end
+
+def subtract(num1, num2)
+    res = num1 - num2
+    return res
+end
+
+def sum(array)
+    res = 0
+    array.each{ |n|
+        res = res + n
+    }
+    return res
+end
+
+def multiply(array)
+    res = 1
+    array.each { |n|
+        res = res * n
+    }
+    return res
+end
+
+def power(num1, num2)
+    res = num1 ** num2
+    return res
+end
+
+def factorial(num)
+    fact = 1
+    i = 1
+
+    until i > num
+        fact = i * fact
+        i = i + 1
+    end
+
+    return fact
+end
