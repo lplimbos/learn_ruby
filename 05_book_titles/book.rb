3232f99e5b66605e9b7b5f9d5c3f67fbd0e5c416
lplimbos <lplimbos@gmail.com>
2020-02-17 13:35:07 -0600

    Solved 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+attr_reader :title
+
+def title=string
+    small_words = %w[a an and if or in the of]
+    words = string.split(" ")
+
+    words.each do |word|
+        word.capitalize! unless small_words.include? word
+    end
+
+    words[0].capitalize!
+    @title = words.join(" ")
+end
+
 end
